e965444c7e719836cd927c00e4a9f229434aa416
tegshjargalaa <jagainaa21@gmail.com>
2021-03-05 15:56:58 -0800

    implemented firebase utils, included ability to store authenticated users into our firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,30 @@ const config = {
     appId: "1:888991162482:web:23b4b2daf8070c2a56a2eb"
   }
 
+  export const createUserProfileDocument = async (userAuth, additionalData)=> {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if(!snapShot.exists){
+      const { displayName, email } = userAuth;
+      const createAt =  new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createAt,
+          ...additionalData
+        })
+      } catch (error){
+        console.log('error creating user', error.message);
+      }
+
+    }
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
-import SignIn from '../../components/sign-in/sign-in.component'
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
 import './sign-in-sign-up.styles.scss'
 
 export default function SignInAndSignUpPage() {
     return (
         <div className='sign-in-sign-up'>
             <SignIn/>
+            <SignUp/>
         </div>
     )
 }
